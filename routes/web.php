c670f7edb3f4c4bbbfc9fdf1f42a789243855ce4
Techiveet <info@techhiveet.com>
2024-09-09 13:27:37 +0300

    authentication added

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -31,23 +30,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-=======
-Route::group([
-    'prefix' => 'to_do_lists',
-], function () {
-    Route::get('/', [ToDoListsController::class, 'index'])
-         ->name('to_do_lists.to_do_list.index');
-    Route::get('/create', [ToDoListsController::class, 'create'])
-         ->name('to_do_lists.to_do_list.create');
-    Route::get('/show/{toDoList}',[ToDoListsController::class, 'show'])
-         ->name('to_do_lists.to_do_list.show')->where('id', '[0-9]+');
-    Route::get('/{toDoList}/edit',[ToDoListsController::class, 'edit'])
-         ->name('to_do_lists.to_do_list.edit')->where('id', '[0-9]+');
-    Route::post('/', [ToDoListsController::class, 'store'])
-         ->name('to_do_lists.to_do_list.store');
-    Route::put('to_do_list/{toDoList}', [ToDoListsController::class, 'update'])
-         ->name('to_do_lists.to_do_list.update')->where('id', '[0-9]+');
-    Route::delete('/to_do_list/{toDoList}',[ToDoListsController::class, 'destroy'])
-         ->name('to_do_lists.to_do_list.destroy')->where('id', '[0-9]+');
-});
->>>>>>> 4bea2b12205abd1c30ba3314f7786a415d8267fe
